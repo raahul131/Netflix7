2eb53b454c27492bdc3d23f4e432980ba71a650b
Rahul R Yadav <rryadav2025@gmail.com>
2023-12-11 09:29:47 +0545

    billboard section added

---
+++ src/components/billboard/BillBoard.tsx
@@ -0,0 +1,13 @@
+import Trending from "../movies/trending/Trending";
+import VideoTitle from "./VideoTitle";
+
+const BillBoard = () => {
+  return (
+    <div>
+      <VideoTitle />
+      <Trending />
+    </div>
+  );
+};
+
+export default BillBoard;
