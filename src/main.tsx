818794851300d691cb84df51fbc2395f91c378c4
Rahul R Yadav <rryadav2025@gmail.com>
2023-12-11 09:28:37 +0545

    routing of details page

--- src/main.tsx
+++ src/main.tsx
@@ -10,12 +10,14 @@ import {
 import Layout from "./Layout.tsx";
 import SignUp from "./components/auth/signup/SignUp.tsx";
 import Auth from "./components/auth/Auth.tsx";
-import BillBoard from "./billboard/BillBoard.tsx";
+import BillBoard from "./components/billboard/BillBoard.tsx";
+import Details from "./components/details/movieDetails/trending/Details.tsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route path="/" element={<BillBoard />} />
+      <Route path="/movie/:id" element={<Details />} />
       <Route path="/signin" element={<Auth />} />
       <Route path="/signup" element={<SignUp />} />
     </Route>
