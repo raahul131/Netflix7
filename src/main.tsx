8ea935500c44308944c3f06f6cf98604f919b057
Rahul R Yadav <rryadav2025@gmail.com>
2023-12-20 16:59:27 +0545

    some routing

--- src/main.tsx
+++ src/main.tsx
@@ -11,13 +11,18 @@ import Layout from "./Layout.tsx";
 import SignUp from "./components/auth/signup/SignUp.tsx";
 import Auth from "./components/auth/Auth.tsx";
 import BillBoard from "./components/billboard/BillBoard.tsx";
-import Details from "./components/details/movieDetails/trending/Details.tsx";
+import Details from "./components/common/Details.tsx";
+import Trailer from "./components/common/Trailer.tsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route path="/" element={<BillBoard />} />
       <Route path="/trendingmovie/:id" element={<Details />} />
+      <Route path="/upcomingmovies/:id" element={<Details />} />
+      <Route path="/topratedmovie/:id" element={<Details />} />
+      <Route path="/popularmovie/:id" element={<Details />} />
+      <Route path="/trailer/:id" element={<Trailer />} />
       <Route path="/signin" element={<Auth />} />
       <Route path="/signup" element={<SignUp />} />
     </Route>
