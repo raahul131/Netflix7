cae1616868ace86fb5e8d3cb4d79f3d761d10774
Rahul R Yadav <rryadav2025@gmail.com>
2023-12-25 10:39:11 +0545

    series added in billboard

--- src/components/billboard/BillBoard.tsx
+++ src/components/billboard/BillBoard.tsx
@@ -2,6 +2,9 @@ import PopularMovies from "../movies/popular/PopularMovies";
 import TopRatedMovies from "../movies/top  rated/TopRated";
 import Trending from "../movies/trending/Trending";
 import Upcoming from "../movies/upcoming/Upcoming";
+import AiringTodayTV from "../series/airing today/AiringTodayTV";
+import Popular from "../series/popular/Popular";
+import TopRated from "../series/top rated/TopRated";
 import VideoTitle from "./VideoTitle";
 
 const BillBoard = () => {
@@ -9,9 +12,20 @@ const BillBoard = () => {
     <div>
       <VideoTitle />
       <Trending />
+
+      {/* TV'S */}
+      <TopRated />
+
       <PopularMovies />
+
+      {/* TV'S */}
+      <AiringTodayTV />
+
       <Upcoming />
       <TopRatedMovies />
+
+      {/* TV'S */}
+      <Popular />
     </div>
   );
 };
