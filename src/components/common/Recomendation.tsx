afde7a6ae3bb21f8584427d4289ab690e0889d60
Rahul R Yadav <rryadav2025@gmail.com>
2023-12-25 10:40:24 +0545

    fallback image incase of image error

--- src/components/common/Recomendation.tsx
+++ src/components/common/Recomendation.tsx
@@ -1,4 +1,6 @@
 import { IMG_CDN_URL } from "../../utils/constants";
+import FALLBACKIMAGE from "../../assets/hero.jpg";
+import { SyntheticEvent } from "react";
 
 interface DetailsProps {
   title: string;
@@ -6,12 +8,17 @@ interface DetailsProps {
 }
 
 const Recomendation = ({ title, poster }: DetailsProps) => {
+  const handleErrorImage = (e: SyntheticEvent<HTMLImageElement, Event>) => {
+    e.currentTarget.onerror = null;
+    e.currentTarget.src = FALLBACKIMAGE;
+  };
   return (
     <div className="min-w-[208px]">
       <img
         src={IMG_CDN_URL + poster}
         alt="No image available"
         className="rounded-md object-cover w-52 border-[1px] border-white/30 text-white text-center"
+        onError={handleErrorImage}
       />
       <h1 className="text-white text-xs font-medium mt-2">{title}</h1>
     </div>
--- src/components/common/Related.tsx
+++ src/components/common/Related.tsx
@@ -1,4 +1,6 @@
 import { IMG_CDN_URL } from "../../utils/constants";
+import FALLBACKIMAGE from "../../assets/hero.jpg";
+import { SyntheticEvent } from "react";
 
 interface DetailsProps {
   title: string;
@@ -6,12 +8,18 @@ interface DetailsProps {
 }
 
 const Related = ({ title, poster }: DetailsProps) => {
+  const handleErrorImage = (e: SyntheticEvent<HTMLImageElement, Event>) => {
+    e.currentTarget.onerror = null;
+    e.currentTarget.src = FALLBACKIMAGE;
+  };
+
   return (
     <div className="min-w-[208px]">
       <img
         src={IMG_CDN_URL + poster}
         alt="No image available"
         className="rounded-md object-cover border-[1px] border-white/30 text-white text-center"
+        onError={handleErrorImage}
       />
       <h1 className="text-white text-xs font-medium mt-2">{title}</h1>
     </div>
